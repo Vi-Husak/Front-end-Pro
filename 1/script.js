0d7925236df80d9d7c07e38d929c083553d4ac54
Vi-Husak <husakvitalii7@gmail.com>
2024-01-19 15:40:53 +0200

    The Homework 1 is done

---
+++ 1/script.js
@@ -0,0 +1,10 @@
+/*
+Треба реалізувати просту програму з наступними умовами:
+    За допомогою prompt запитати ім'я користувача.
+    За допомогою alert вивести "Hello, John! How are you?" , де John це те, що ввів користувач
+*/
+
+const name = prompt("What is your name?");
+const hello = `Hello, ${name}! How are you?`;
+
+alert(hello);
